9a36e077b15d64eeadbaa8893be8467b1084d911
Rajkishor Patra <53007802+imraj569@users.noreply.github.com>
2022-03-29 09:27:05 +0530

    Add files via upload

--- InstaLoader.py
+++ InstaLoader.py
@@ -1,3 +1,4 @@
+from ast import While
 import requests
 import re
 import colorama
@@ -5,11 +6,14 @@ from colorama import Fore
 colorama.init(autoreset=True)
 
 def get_response(url):
-    header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36"}
-    r = requests.get(url,headers=header)
-    while r.status_code != 200:
+    try:
+        header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36"}
         r = requests.get(url,headers=header)
-    return r.text
+        while r.status_code != 200:
+            r = requests.get(url,headers=header)
+        return r.text
+    except:
+        print('somthing went wrong !')
 
 def prepare_urls(matches):
     return list({match.replace("\\u0026", "&") for match in matches})
@@ -30,22 +34,25 @@ Just past The instagram post link and Download
 ------------------------------------------------------------------                            
 ''')
 
-url = input(Fore.BLUE+'Enter Instagram URL: ')
-response = get_response(url)
+def l(): 
+    url = input(Fore.BLUE+'Enter Instagram URL: ')
+    response = get_response(url)
+    
+    vid_matches = re.findall('"video_url":"([^"]+)"', response)
+    pic_matches = re.findall('"display_url":"([^"]+)"', response)
 
-vid_matches = re.findall('"video_url":"([^"]+)"', response)
-pic_matches = re.findall('"display_url":"([^"]+)"', response)
+    vid_urls = prepare_urls(vid_matches)
+    pic_urls = prepare_urls(pic_matches)
 
-vid_urls = prepare_urls(vid_matches)
-pic_urls = prepare_urls(pic_matches)
+    if vid_urls:
+        print(Fore.CYAN+'\nDetected Videos:\n{0}'.format('\n'.join(vid_urls)))
 
-if vid_urls:
-    print(Fore.CYAN+'\nDetected Videos:\n{0}'.format('\n'.join(vid_urls)))
+    if pic_urls:
+        print(Fore.LIGHTYELLOW_EX+'\nDetected Pictures:\n{0}'.format('\n'.join(pic_urls)))
 
-if pic_urls:
-    print(Fore.LIGHTYELLOW_EX+'\nDetected Pictures:\n{0}'.format('\n'.join(pic_urls)))
+    if not (vid_urls or pic_urls):
+        print(Fore.RED+'Could not recognize the media in the provided URL.')
 
-if not (vid_urls or pic_urls):
-    print(Fore.RED+'Could not recognize the media in the provided URL.')
-
-  
+if __name__ == '__main__':
+    while True: 
+        l()
