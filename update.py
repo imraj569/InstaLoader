0b70a4743f685e856959d40c56709834437cb7f8
Rajkishor Patra <53007802+imraj569@users.noreply.github.com>
2022-06-26 22:35:16 +0530

    Add files via upload

--- update.py
+++ update.py
@@ -7,7 +7,8 @@ import os , sys ,time
 
 os.system('clear')
 print(Fore.GREEN+'Updating InstaLoader please wait ...')
-os.system('rm -rf InstaLoader')
+folder = '/data/data/com.termux/files/home/InstaLoader'
+os.rmdir(folder)
 os.system('git clone https://github.com/imraj569/InstaLoader.git')
 os.system('clear')
 print(Fore.GREEN+'InstaLoader updated successfully...')
